f0b158cd194bb1c28117742e3ce59d34cbdc7b35
Adolfo <yml.aldana@gmail.com>
2020-07-11 17:54:53 -0400

    Uso de useState y onChange en Pregunta.js

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,9 @@
 // @flow
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 export const Pregunta = () => {
+  const [cantidad, guardarCantidad] = useState(0);
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
@@ -10,6 +12,7 @@ export const Pregunta = () => {
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={(e) => guardarCantidad(parseInt(e.target.value, 10))}
         />
 
         <input
