104ffc260e514b3e8d01a254212d5119380a689e
Adolfo <yml.aldana@gmail.com>
2020-07-11 17:44:29 -0400

    Componente creado y contenido de pregunta presupuesto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Pregunta } from './components/Pregunta';
 
 function App() {
   return (
     <div className="container">
       <header>
         <h1>Gasto Mensual</h1>
+
+        <div className="contenido-principal cotenido">
+          <Pregunta />
+        </div>
       </header>
     </div>
   );
