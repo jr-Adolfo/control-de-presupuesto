d94585f82afd97ca44e5c90fa3f7d9c7a547930f
Adolfo <yml.aldana@gmail.com>
2020-07-12 20:03:11 -0400

    restando del presupuesto al añadir un gasto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Pregunta } from "./components/Pregunta";
 import Formulario from "./components/Formulario";
 import Listado from "./components/Listado";
@@ -9,10 +9,24 @@ function App() {
   const [restante, guardarRestante] = useState(0);
   const [mostrarpregunta, actualizarPregunta] = useState(true);
   const [gastos, guardarGastos] = useState([]);
+  const [gasto, guardarGasto] = useState({});
+  const [creargasto, guardarCrearGasto] = useState(false);
 
-  const agregandoNuevoGasto = (gasto) => {
-    guardarGastos([...gastos, gasto]);
-  };
+  useEffect(() => {
+    if (creargasto) {
+      //Agregar el nuevo presupuesto
+      guardarGastos([...gastos, gasto]);
+
+      //resta del presupuesto actual
+      const presupuestoRestante = restante - gasto.cantidad;
+      guardarRestante(presupuestoRestante);
+    }
+
+    // guardarCrearGasto(false);
+  }, [gasto]);
+  // const agregandoNuevoGasto = (gasto) => {
+  //   guardarGastos([...gastos, gasto]);
+  // };
 
   return (
     <div className="container">
@@ -29,7 +43,10 @@ function App() {
           ) : (
             <div className="row">
               <div className="one-half column">
-                <Formulario agregandoNuevoGasto={agregandoNuevoGasto} />
+                <Formulario
+                  guardarGasto={guardarGasto}
+                  guardarCrearGasto={guardarCrearGasto}
+                />
               </div>
               <div className="one-half column">
                 <Listado gastos={gastos} />
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Error from "./Error";
 import shortid from "shortid";
 
-const Formulario = ({ agregandoNuevoGasto }) => {
+const Formulario = ({ guardarGasto, guardarCrearGasto }) => {
   const [nombre, guardarNombre] = useState("");
   const [cantidad, guardarCantidad] = useState(0);
   const [error, guardarError] = useState(false);
@@ -23,7 +23,9 @@ const Formulario = ({ agregandoNuevoGasto }) => {
       id: shortid.generate(),
     };
 
-    agregandoNuevoGasto(gasto);
+    guardarGasto(gasto);
+    guardarCrearGasto(true);
+
     //Limpiando form
     guardarNombre("");
     guardarCantidad(0);
